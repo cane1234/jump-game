6b7906fb269c9f99d1d4d1a7fd550d7f4187cf21
Stefan Subotic <subotics009@gmail.com>
2021-02-13 13:00:20 +0100

    basic player object and movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rigidBody2D;
+
+    public float speed;
+    public float jumpForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        Vector2 movement = new Vector2(moveHorizontal, 0);
+
+        rigidBody2D.AddForce(movement * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidBody2D.AddForce(new Vector2(0, jumpForce));
+        }
+    }
+
+}
