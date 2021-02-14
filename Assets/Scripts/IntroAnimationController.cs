4f03e6e935425058e1e4df735be4c99b0a639fd6
Stefan Subotic <subotics009@gmail.com>
2021-02-14 12:51:53 +0100

    Revert "Revert "Intro Animation""
    This reverts commit fbca0af2b2ce591125b24bf9549fcf40ac5893de.

---
+++ Assets/Scripts/IntroAnimationController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class IntroAnimationController : MonoBehaviour
+{
+    [NonSerialized]
+    public UnityEvent IntroAnimationCompleteEvent;
+
+    private void Start()
+    {
+        IntroAnimationCompleteEvent = new UnityEvent();    
+    }
+
+    public void IntroAnimationComplete()
+    {
+        IntroAnimationCompleteEvent.Invoke();
+    }
+}
